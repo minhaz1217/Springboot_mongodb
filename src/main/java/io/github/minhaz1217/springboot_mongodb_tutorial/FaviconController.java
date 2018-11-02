3b874720ba05dd862fafcb99e0890470a5837cb0
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2018-11-02 14:01:44 +0600

    added favicon remove class

---
+++ src/main/java/io/github/minhaz1217/springboot_mongodb_tutorial/FaviconController.java
@@ -0,0 +1,14 @@
+package io.github.minhaz1217.springboot_mongodb_tutorial;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FaviconController {
+
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    void returnNoFavicon() {
+    }
+}
