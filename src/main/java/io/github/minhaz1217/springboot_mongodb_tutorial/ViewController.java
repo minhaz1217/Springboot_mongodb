99f88d54004b86547b71a283f6605a9e71ba09e6
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2018-11-02 13:50:58 +0600

    removed the request mapping for /

--- src/main/java/io/github/minhaz1217/springboot_mongodb_tutorial/ViewController.java
+++ src/main/java/io/github/minhaz1217/springboot_mongodb_tutorial/ViewController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+//@RequestMapping("/")
 public class ViewController {
     @RequestMapping("/")
     @ResponseBody
